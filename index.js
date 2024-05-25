91d166d020160d62238f844a49b5f8312169f7ba
amarvir <amarvirgill06@gmail.com>
2024-05-24 23:23:16 -0300

    added cors

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ import { folderRoute } from "./routes/folderRoute.js";
 import { taskRouter } from "./routes/taskRoute.js";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://notes-app-nine-sandy.vercel.app"],
+    methods: ["GET, POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/auth", userRouter);
 app.use("/addFolder", folderRoute);
